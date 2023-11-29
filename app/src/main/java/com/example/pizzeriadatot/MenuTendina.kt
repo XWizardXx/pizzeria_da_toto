e33a42e146217de20c0d963a7d3a63480703cd3b
CarlettoDeiSofficini <parrottacarlo00@gmail.com>
2023-11-29 15:30:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pizzeriadatot/MenuTendina.kt
@@ -0,0 +1,13 @@
+package com.example.pizzeriadatot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuTendina : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_tendina)
+    }
+}
