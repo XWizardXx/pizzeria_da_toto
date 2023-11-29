43dc0cd5c0d91b8f807abdf614a193f88098463a
CarlettoDeiSofficini <parrottacarlo00@gmail.com>
2023-11-29 19:49:08 +0100

    ho provato a mettere un bottone immagine, non è andata bene

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pizzeriadatot"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pizzeriadatot"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
