8e7dff980dd82d92c814212e36cd4c5637054fd4
CarlettoDeiSofficini <parrottacarlo00@gmail.com>
2023-11-29 22:35:12 +0100

    fixato bottone toHome

--- app/src/main/java/com/example/pizzeriadatot/MenuTendina.kt
+++ app/src/main/java/com/example/pizzeriadatot/MenuTendina.kt
@@ -1,6 +1,6 @@
 package com.example.pizzeriadatot
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,12 +8,15 @@ import android.widget.ImageButton
 
 class MenuTendina : AppCompatActivity()
 {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_tendina)
 
-        val goBack : ImageButton = findViewById<ImageButton>(R.id.imageButton9)
+        val goBack : ImageButton = findViewById<ImageButton>(R.id.toHome)
+        goBack.setOnClickListener {
+            val toHome = Intent(this, HomePage::class.java)
+            startActivity(toHome)
+        }
     }
 }
