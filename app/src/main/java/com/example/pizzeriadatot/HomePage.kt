f7d80dd3ea192860df40c5d40a5bb53bdc06e2b0
Gory-git <61806971+Gory-git@users.noreply.github.com>
2023-11-29 21:38:24 +0100

    sostituito bottone menu con bottone immagine -greg

--- app/src/main/java/com/example/pizzeriadatot/HomePage.kt
+++ app/src/main/java/com/example/pizzeriadatot/HomePage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 
 
@@ -14,7 +15,7 @@ class HomePage : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        val menu : Button = findViewById<Button>(R.id.menu)
+        val menu: ImageButton = findViewById(R.id.menu)
         menu.setOnClickListener {
             val toMenu = Intent(this, MenuTendina::class.java)
             startActivity(toMenu)
