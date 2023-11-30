0e498075f58c7acaf666cbbf732e402efdfbce42
Gory-git <61806971+Gory-git@users.noreply.github.com>
2023-11-30 10:33:23 +0100

    aggiunti bottoni nel menu a tendina e aggiunte le varie activitys a esso collegato - greg

--- app/src/main/java/com/example/pizzeriadatot/MenuTendina.kt
+++ app/src/main/java/com/example/pizzeriadatot/MenuTendina.kt
@@ -13,10 +13,24 @@ class MenuTendina : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_tendina)
 
-        val goBack : ImageButton = findViewById<ImageButton>(R.id.toHome)
+        val goBack : ImageButton = findViewById(R.id.toHome)
         goBack.setOnClickListener {
             val toHome = Intent(this, HomePage::class.java)
             startActivity(toHome)
         }
+
+        val userPage : Button = findViewById(R.id.toUser)
+        userPage.setOnClickListener {
+            val toUser = Intent(this, Utente::class.java)
+            startActivity(toUser)
+        }
+
+        val menu : Button = findViewById(R.id.toMenu)
+        menu.setOnClickListener {
+            val toMenu = Intent(this, Menu::class.java)
+            startActivity(toMenu)
+        }
+
+        val
     }
 }
---
+++ app/src/main/java/com/example/pizzeriadatot/Utente.kt
@@ -0,0 +1,23 @@
+package com.example.pizzeriadatot
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Utente : AppCompatActivity() {
+    val loggato : Boolean = false;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_utente)
+        if (!loggato)
+        {
+            val toLogin = Intent(this, Login::class.java)
+            startActivity(toLogin)
+        }
+        else
+        {
+            val toUserPage = Intent(this, UserPage::class.java)
+            startActivity(toUserPage) 
+        }
+    }
+}
