77ccf960aae197d7a7c0e04b2182d3441191a209
CarlettoDeiSoffocotti <blackgamer034@gmail.com>
2023-11-30 16:13:27 +0100

    gui dell'activity login

--- app/src/main/java/com/example/pizzeriadatot/Login.kt
+++ app/src/main/java/com/example/pizzeriadatot/Login.kt
@@ -2,9 +2,16 @@ package com.example.pizzeriadatot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class Login : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class Login : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        val login : Button = findViewById(R.id.login)
+        val
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
