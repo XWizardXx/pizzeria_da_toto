8a5d6c6327e542e1ac81678ca948e33c6162e8fb
CarlettoDeiSoffocotti <blackgamer034@gmail.com>
2023-12-03 14:56:45 +0100

    cose 2 perchè gregorio puzza

--- app/src/main/java/com/example/pizzeriadatot/SignIn.kt
+++ app/src/main/java/com/example/pizzeriadatot/SignIn.kt
@@ -7,11 +7,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.pizzeriadatot.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SignIn : AppCompatActivity()
 {
-    private lateinit var binding
+    private lateinit var binding: ActivitySignInBinding
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
